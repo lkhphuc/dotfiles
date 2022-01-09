9c3fc9add33210f1d77182e0cadd3fab301a332b
Phúc Lê Khắc <phuc.lkh@gmail.com>
2022-01-09 23:25:37 +0700

    wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,31 @@
 local wezterm = require 'wezterm';
 
 return {
+  ssh_domains = {
+    {
+      name = "gpu",
+      remote_address = "gpu-et",
+      username = "phuc",
+    },
+    {
+      name = "carles",
+      remote_address = "carles",
+      username = "phuc"
+    },
+  },
+
   font = wezterm.font_with_fallback({
     {family="Rec Mono Duotone"},
-    {family="JetBrainsMono Nerd Font Mono"}
+    {family="Victor Mono"},
+    {family="Libertinus Mono"},
+    {family="Hack Nerd Font"},
+    {family="JetBrainsMono Nerd Font"}
   }),
-  font_size = 15,
-  color_scheme = "OneHalfDark",
+  font_size = 14,
+  color_scheme = "OneDark",
+
+  keys = {
+    {key="s", mods="CMD", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}
+    }
+  }
 }
