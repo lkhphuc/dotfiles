6b1f12a7b3b951191cddb8929f3f52a2bfbf1607
Phúc Lê Khắc <phuc.lkh@gmail.com>
2022-01-25 12:16:46 +0700

    zsh and deploy

--- deploy.sh
+++ deploy.sh
@@ -1,37 +1,3 @@
-prompt_install() {
-	echo -n "$1 is not installed. Would you like to install it? (y/n) " >&2
-	old_stty_cfg=$(stty -g)
-	stty raw -echo
-	answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
-	stty $old_stty_cfg && echo
-	if echo "$answer" | grep -iq "^y" ;then
-		if [ -x "$(command -v apt-get)" ]; then
-			sudo add-apt-repository $2
-			sudo apt update
-			sudo apt install $1 -y
-
-		elif [ -x "$(command -v brew)" ]; then
-			brew install $1
-
-		elif [ -x "$(command -v pacman)" ]; then
-			sudo pacman -S $1
-
-		else
-			echo "Package manager not detected! Please install $1 on your own and run this deploy script again. Feel free to make a pull request at https://github.com/lkhphuc/dotfiles :)"
-		fi
-	fi
-}
-
-check_for_software() {
-	echo "Checking to see if $1 is installed"
-	if ! [ -x "$(command -v $1)" ]; then
-		prompt_install $2
-	else
-		echo "$1 is installed."
-	fi
-	echo
-}
-
 check_default_shell() {
 	if [ -z "${SHELL##*zsh*}" ] ;then
 			echo "Default shell is zsh."
@@ -49,12 +15,9 @@ check_default_shell() {
 	fi
 }
 
-echo "We're going to do the following:"
-echo "1. Check to make sure you have zsh, neovim, and tmux installed"
-echo "2. We'll help you install them if you don't"
-echo "3. We're going to check to see if your default shell is zsh"
-echo "4. We'll try to change it if it's not"
-
+echo "1. install HomeBrew package manager."
+echo "2. install various other software."
+echo "3. Make default shell to be zsh."
 echo "Let's get started? (y/n)"
 old_stty_cfg=$(stty -g)
 stty raw -echo
@@ -67,19 +30,33 @@ else
 	exit 0
 fi
 
+###### HOMEBREW ############
+echo "Installed homebrew if not already."
+if ! [ -x "brew" ]; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+###########################
 
-check_for_software zsh
-# check_for_software tmux
-check_for_software nvim neovim ppa:neovim-ppa/unstable
-check_for_software lazygit lazygit ppa:lazygit-team/release
-check_for_software rg ripgrep
-check_for_software thefuck thefuck
-check_for_software fd fd
-check_for_software bat bat
-check_for_software lf lf
-check_for_software exa exa
+
+brew install bat
+brew install exa
+brew install git-delta
+brew tap tgotwig/linux-dust && brew install dust
+brew install duf
+brew install ripgrep
+brew install thefuck
+brew install fd
+brew tap cantino/mcfly && brew install mcfly
+brew install zoxide
+brew install neovim
+brew install lazygit
+brew install lf
 
 check_default_shell
+
+echo
+echo "Optionally install LunarVim by running this command \\
+ LV_BRANCH=rolling bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/rolling/utils/installer/install.sh) "
 
 echo
 echo -n "Would you like to backup your current dotfiles? (y/n) "
@@ -90,15 +67,11 @@ stty $old_stty_cfg
 if echo "$answer" | grep -iq "^y" ;then
 	mv ~/.zshrc ~/.zshrc.old
 	mv ~/.tmux.conf ~/.tmux.conf.old
-	mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.old
 else
 	echo -e "\nNot backing up old dotfiles."
 fi
 
-echo "Installing Lunarvim"
-LV_BRANCH=rolling bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/rolling/utils/installer/install.sh)
-
-printf "source '$HOME/.config/zsh/zshrc'" > $HOME/.zshrc
+printf "source '$HOME/.config/zsh/zshrc'" >> $HOME/.zshrc
 
 echo
 echo "Please log out and log back in for default shell to be initialized."
