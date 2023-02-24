7d2890b900674397c2231f0445dd5aa1279ebfa2
Phúc Lê Khắc <phuc.lkh@gmail.com>
2023-02-24 12:35:59 +0000

    add rust

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -4,6 +4,7 @@ return {
   { import = "lazyvim.plugins.extras.lang.typescript" },
   -- add jsonls and schemastore ans setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "plugins.extras.rust" },
 
   -- use mini.starter instead of alpha
   -- { import = "lazyvim.plugins.extras.ui.mini-starter" },
