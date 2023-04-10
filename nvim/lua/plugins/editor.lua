71056c675d559468aface79b8af746278a851ea8
Phúc Lê Khắc <phuc.lkh@gmail.com>
2023-04-10 15:40:12 +0100

    nvim: use mini.cursorword instead of illuminate

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -80,4 +80,11 @@ return {
     },
     config = function(_, opts) require("mini.bracketed").setup(opts) end,
   },
+  { "RRethy/vim-illuminate", enabled = false },
+  { "echasnovski/mini.cursorword",
+    config = function(_, opts)
+      require("mini.cursorword").setup(opts)
+    end,
+    event = "VeryLazy"
+  },
 }
