4088b7844159259be6e58824579730f8928a2247
Phúc Lê Khắc <phuc.lkh@gmail.com>
2023-08-16 07:02:46 +0200

    nvim: cmp-buffer only when no lsp

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -92,6 +92,7 @@ return {
       { name = "jupyter", priority = 750 },
       { name = "nvim_lsp" },
       { name = "luasnip" },
+    },{
       { name = "buffer",
         -- option = { get_bufnrs = function () return vim.api.nvim_list_bufs() end }
       },
