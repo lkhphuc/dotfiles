e9202a618abdf2ce3bb13a8164016b02ec9d3253
Phúc Lê Khắc <phuc.lkh@gmail.com>
2023-10-12 17:57:33 +0200

    nvim: python fstring autocommand

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -37,7 +37,47 @@ vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
   end,
 })
 
+----------------- python -----------------
+-- From nvim-puppetteer
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  pattern = { "*.py" },
+  callback = function()  -- Auto f-string on typing {
+    local function replaceNodeText(node, text)
+      local start_row, start_col, end_row, end_col = node:range()
+      local lines = vim.split(text, "\n")
+      vim.cmd.undojoin() -- make undos ignore the next change, see issue #8
+      vim.api.nvim_buf_set_text(0, start_row, start_col, end_row, end_col, lines)
+    end
+
+    local node = vim.treesitter.get_node()
+    if not node then return end
+
+    local str_node
+    if node:type() == "string" then
+      str_node = node
+    elseif node:type():find("^string_") then
+      str_node = node:parent()
+    elseif node:type() == "escape_sequence" then
+      str_node = node:parent():parent()
+    else
+      return
+    end
+
+    local text = vim.treesitter.get_node_text(str_node, 0)
+    if text == "" then return end -- don't convert empty strings, user might want to enter sth
+
+    local isFString = text:find("^f")
+    local hasBraces = text:find("{.-}")
+
+    if not isFString and hasBraces then
+      replaceNodeText(str_node, "f" .. text)
+    elseif isFString and not hasBraces then
+      text = text:sub(2)
+      replaceNodeText(str_node, text)
+    end
+  end,
+})
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
   pattern = { "*.ipynb" },
-  command = "set commentstring=# %s"
+  command = "set commentstring=# %s",
 })
