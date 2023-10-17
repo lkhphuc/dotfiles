3372deffd64cb35e3597a5fa31c08098bb75af2f
Phúc Lê Khắc <phuc.lkh@gmail.com>
2023-10-17 10:27:09 +0200

    feat: refactor sign column

--- nvim/lua/config/util.lua
+++ nvim/lua/config/util.lua
@@ -3,47 +3,56 @@ local M_lz = require("lazyvim.util.ui")
 M = {}
 M.statuscolumn = function()
   local win = vim.g.statusline_winid
-  if vim.wo[win].signcolumn == "no" then
-    return ""
-  end
   local buf = vim.api.nvim_win_get_buf(win)
+  local is_file = vim.bo[buf].buftype == ""
+  local show_signs = vim.wo[win].signcolumn ~= "no"
 
+  local components = { "", "", "" } -- left, middle, right
+
+  if show_signs then
   ---@type Sign?,Sign?,Sign?
-  local sign, gitsign, fold
-  for _, s in ipairs(M_lz.get_signs(buf, vim.v.lnum)) do
-    if s.name:find("GitSign") then
-      gitsign = s
+    local sign, gitsign, fold
+    for _, s in ipairs(M_lz.get_signs(buf, vim.v.lnum)) do
+      if s.name and s.name:find("GitSign") then
+        gitsign = s
+      else
+        sign = s
+      end
+    end
+
+    vim.api.nvim_win_call(win, function()
+      if vim.fn.foldclosed(vim.v.lnum) >= 0 then
+        fold = { text = vim.opt.fillchars:get().foldclose or "", texthl = "Folded" }
+      elseif vim.fn.foldlevel(vim.v.lnum) > vim.fn.foldlevel(vim.v.lnum - 1) then
+        fold = { text = vim.opt.fillchars:get().foldopen or "", texthl = "Folded" }
+      end
+    end)
+
+    local mark = M_lz.get_mark(buf, vim.v.lnum)
+    if vim.v.virtnum ~= 0 then
+      -- Don't duplicate sign on virtual line
+      sign = nil
     else
-      sign = s
+      sign = sign or mark or fold
+    end
+    -- except for gitsign's indicator line
+    components[2] = M_lz.icon(sign or gitsign)
+  end
+
+  -- Numbers in Neovim are weird
+  -- They show when either number or relativenumber is true
+  local is_num = vim.wo[win].number
+  local is_relnum = vim.wo[win].relativenumber
+  if (is_num or is_relnum) and vim.v.virtnum == 0 then
+    if vim.v.relnum == 0 then
+      components[1] = is_num and "%l" or "%r" -- the current line
+    else
+      components[1] = is_relnum and "%r" or "%l" -- other lines
     end
   end
+  components[1] = "%=" .. components[1] .. " " -- right align
 
-  vim.api.nvim_win_call(win, function()
-    if vim.fn.foldclosed(vim.v.lnum) >= 0 then
-      fold = { text = vim.opt.fillchars:get().foldclose or "", texthl = "Folded" }
-    elseif vim.fn.foldlevel(vim.v.lnum) > vim.fn.foldlevel(vim.v.lnum - 1) then
-      fold = { text = vim.opt.fillchars:get().foldopen or "", texthl = "Folded" }
-    end
-  end)
-
-  local mark = M_lz.get_mark(buf, vim.v.lnum)
-
-  if vim.v.virtnum ~= 0 then
-    sign = nil
-  else
-    sign = sign or mark or fold
-  end
-
-  local nu = ""
-  if vim.wo[win].number and vim.v.virtnum == 0 then
-    nu = vim.wo[win].relativenumber and vim.v.relnum ~= 0 and vim.v.relnum or vim.v.lnum
-  end
-
-  return table.concat({
-    [[%=]],
-    nu .. " ",
-    M_lz.icon(sign or gitsign),
-  }, "")
+  return table.concat(components, "")
 end
 
 return M
