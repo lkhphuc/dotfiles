fe703e260ddd51afc393a046c2b299f57c1c01f1
Phúc Lê Khắc <phuc.lkh@gmail.com>
2024-08-26 15:01:39 +0400

    repl: switch back send-to.nvim

--- nvim/lua/plugins/repl.lua
+++ nvim/lua/plugins/repl.lua
@@ -5,8 +5,42 @@
 local cell_marker = [[# %%]]
 
 return {
+  {
+    "mtikekar/nvim-send-to-term",
+    cmd = { "SendTo", "SendHere" },
+    init = function()
+      vim.g.send_disable_mapping = true -- dont use default
+      local function send_to_wez(opts)
+        local pane_id = opts.args
+        local function send_to_pane(lines)
+          lines = table.concat(lines, "\n"):gsub('"', '\\"') -- Escape double quote since it's used to wrap lines
+          os.execute('wezterm cli send-text --pane-id=' .. pane_id .. ' "' .. lines .. '"')
+          os.execute('wezterm cli send-text --pane-id=' .. pane_id .. ' --no-paste "\r\r"')
+        end
+        vim.g.send_target = { send = send_to_pane, }
+      end
+      vim.api.nvim_create_user_command("SendToWez", send_to_wez, { nargs = 1 })
+      local function send_to_jupyter(opts)
+        if vim.b.jupyter_attached == nil then
+          vim.notify("No jupyter kernel attached")
+          return
+        end
+        vim.g.send_target = { send = function (lines)
+          lines = table.concat(lines, "\n")
+          vim.fn.JupyterExecute(lines)
+        end}
+      end
+      vim.api.nvim_create_user_command("SendToJupyter", send_to_jupyter, {})
+    end,
+    keys = {
+      { "<C-CR>",   "<Plug>Send",    desc = "Send", mode = "n" },
+      { "<S-CR>",   "<Plug>Send'>",  desc = "Send", mode = "v" },
+      { "<S-CR>", "vap<Plug>Send'>", desc = "Send", mode = "n" },
+    },
+  },
   { -- General vim repl code send
     "jpalardy/vim-slime",
+    enable = false,
     init = function()
       vim.g.slime_no_mappings = 1
       vim.g.slime_cell_delimiter = cell_marker
