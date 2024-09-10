97dd5a2d3701c0b693355bbc62e7f6a1f3970cc1
Phúc Lê Khắc <phuc.lkh@gmail.com>
2024-09-10 11:53:28 +0400

    add nvim ai codecompanion

---
+++ nvim/lua/plugins/ai.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- Optional
+      {
+        "stevearc/dressing.nvim", -- Optional: Improves the default Neovim UI
+        opts = {},
+      },
+    },
+    opts = {
+      adapters = {
+        openai = function()
+          return require("codecompanion.adapters").extend("openai", {
+            url = "localhost:8080/v1/chat/completions",
+          })
+        end,
+      },
+    },
+    cmd = { "CodeCompanion" },
+    keys = {
+      { "<leader>aa", "<CMD>CodeCompanionActions<CR>", mode = { "n", "v" }, desc = "AI Actions" },
+      { "<C-,>", "<CMD>CodeCompanionToggle<CR>", mode = { "n", "v" }, desc = "AI chat Toggle" },
+      { "<leader>ac", "<CMD>CodeCompanionAdd<CR>", mode = { "v" }, desc = "AI add to Chat" },
+      { "<leader>ap", "<CMD>'<,'>CodeCompanion<CR>", mode = { "n", "v" }, desc = "AI Prompt" },
+    },
+  },
+}
